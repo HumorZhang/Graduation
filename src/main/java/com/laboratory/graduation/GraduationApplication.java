e14ed6bfe150e34b0f4588a692baa594cac851fc
HumorZhang <373014368@qq.com>
2019-04-04 12:55:12 +0800

    submint person and notes

--- src/main/java/com/laboratory/graduation/GraduationApplication.java
+++ src/main/java/com/laboratory/graduation/GraduationApplication.java
@@ -11,5 +11,5 @@ public class GraduationApplication {
     public static void main(String[] args) {
         SpringApplication.run(GraduationApplication.class, args);
     }
-
+//gGraduation
 }
---
+++ src/main/java/com/laboratory/graduation/component/person.java
@@ -0,0 +1,9 @@
+package com.laboratory.graduation.component;
+
+/**
+ * zgg
+ * 2019/4/4 12:52
+ */
+public class person {
+
+}
